e0007c9ce03c53b7747122590a0338599c8d5818
SoulerSakeR <53467643+SoulerSakeR@users.noreply.github.com>
2023-04-07 23:36:19 +0800

    changes
    1

--- Core/GameLogic/GameLoop.cpp
+++ Core/GameLogic/GameLoop.cpp
@@ -2,13 +2,13 @@
 
 
 
-    void updatePlayer(float deltaTime) {
+void GameLoop::updatePlayer(float deltaTime) {
         // 更新玩家状态-比如位置和执行动作
 
         // ...
     }
 
-    void updateScene(RenderWidget* Rwg) {
+void GameLoop::updateScene(RenderWidget* Rwg) {
         // 更新场景信息-需要和光夫哥和敬哥对接
         // 获取场景信息(光夫哥)并且进行渲染（敬哥）
         // ...       
@@ -30,23 +30,23 @@
 
     }
 
-    void checkCollisions(float deltaTime) {
+void GameLoop::checkCollisions(float deltaTime) {
         // 检查碰撞 和宇靖对接
         // ...
     }
 
-    void updateGameState(float deltaTime) {
+void GameLoop::updateGameState(float deltaTime) {
         // 更新游戏状态
         // 比如玩家的位置
         // ...
     }
 
-    void printDataInfo(float deltaTime) {
+void GameLoop::printDataInfo(float deltaTime) {
         //输出数据信息-测试用
     }
 
 
-        void update(float deltaTime, RenderWidget* Rwg) {
+void GameLoop::update(float deltaTime, RenderWidget* Rwg) {
         // 游戏逻辑更新函数，每帧调用一次
         updatePlayer(deltaTime);
         updateScene(Rwg);
