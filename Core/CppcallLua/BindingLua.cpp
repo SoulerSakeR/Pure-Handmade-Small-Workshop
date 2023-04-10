af45b802de0d37774d9ee25d389b59055a30d93a
chichuhanye <60133082+chichuhanye@users.noreply.github.com>
2023-04-10 16:13:39 +0100

    添加BindingLua.h

--- Core/CppcallLua/BindingLua.cpp
+++ Core/CppcallLua/BindingLua.cpp
@@ -4,49 +4,42 @@
 #include "Core/Debug.h"
 #include "Core/GameObject.h"
 #include "Core/Transform.h"
+#include "Utils/Vector2D.h"
 
 using namespace std;
 
 
-/*
-    Input
-*/
 
-//  InputDetection
-void InputDetectionLua(sol::state lua) {
+void bindAllClasses(sol::state& lua) {
+        
     lua.open_libraries(sol::lib::base, sol::lib::package);
 
+    /*
+    Input
+    */
+
+    //  InputDetection
     lua.new_usertype<InputDetection>("InputDetection",
         // 静态成员函数
         "getKey", &InputDetection::GetKey,
         "getKeyDown", &InputDetection::GetKeyDown,
         "getKeyUp", &InputDetection::GetKeyUp
         );
-}
 
 
-
-
-/*
+    /*
     Core
-*/
+    */
 
-// Debug
-void DebugLua(sol::state lua) {
-    lua.open_libraries(sol::lib::base, sol::lib::package);
-
+    // Debug
     lua.new_usertype<Debug>("Debug",
         // 静态成员函数
         "log", &Debug::log,
         "logError", &Debug::logError
         );
 
-}
 
-//  GameObject
-void GameObjectLua(sol::state lua) {
-    lua.open_libraries(sol::lib::base, sol::lib::package);
-
+    //  GameObject
     lua.new_usertype<GameObject>("GameObject",
         // 构造函数
         sol::constructors<GameObject(std::string, bool)>(),
@@ -70,29 +63,140 @@ void GameObjectLua(sol::state lua) {
         return obj.getComponent(type);
     };
 
-}
 
-//  Transform
-void TransformLua(sol::state lua) {
-    lua.open_libraries(sol::lib::base, sol::lib::package);
-
-    lua.new_usertype<Transform>("Transform",
-        // 构造函数
+    //  Transform
+    lua.new_usertype<Transform>(
+        "Transform",
         sol::constructors<Transform(GameObject*)>(),
+        sol::meta_function::garbage_collect, sol::destructor([](Transform& t) { t.~Transform(); }),
 
-        // 成员变量
-        "localPosition", &Transform::localPosition,
-        "localRotation", &Transform::localRotation,
-        "localScale", &Transform::localScale,
-        "children", &Transform::children,
-        "parent", &Transform::parent,
+        // 获取本地坐标位置
+        "get_localPosition", &Transform::get_localPosition,
 
-        // 成员函数
+        // 获取本地旋转角度
+        "get_localRotation", &Transform::get_localRotation,
+
+        // 获取本地缩放
+        "get_localScale", &Transform::get_localScale,
+
+        // 设置本地坐标位置
+        "set_localPosition", &Transform::set_localPosition,
+
+        // 设置本地旋转角度
+        "set_localRotation", &Transform::set_localRotation,
+
+        // 设置本地缩放
+        "set_localScale", &Transform::set_localScale,
+
+        // 获取世界坐标位置
         "getWorldPosition", &Transform::getWorldPosition,
+
+        // 获取世界旋转角度
         "getWorldRotation", &Transform::getWorldRotation,
+
+        // 获取世界缩放
         "getWorldScale", &Transform::getWorldScale,
+
+        // 朝给定向量移动
         "translate", &Transform::translate
         );
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+/*
+    Input
+*/
+
+//  InputDetection
+//void InputDetectionLua(sol::state lua) {
+//    lua.open_libraries(sol::lib::base, sol::lib::package);
+//
+//    lua.new_usertype<InputDetection>("InputDetection",
+//        // 静态成员函数
+//        "getKey", &InputDetection::GetKey,
+//        "getKeyDown", &InputDetection::GetKeyDown,
+//        "getKeyUp", &InputDetection::GetKeyUp
+//        );
+//}
+
+
+
+
+/*
+    Core
+*/
+
+// Debug
+//void DebugLua(sol::state lua) {
+//    lua.open_libraries(sol::lib::base, sol::lib::package);
+//
+//    lua.new_usertype<Debug>("Debug",
+//        // 静态成员函数
+//        "log", &Debug::log,
+//        "logError", &Debug::logError
+//        );
+//
+//}
+
+//  GameObject
+//void GameObjectLua(sol::state lua) {
+//    lua.open_libraries(sol::lib::base, sol::lib::package);
+//
+//    lua.new_usertype<GameObject>("GameObject",
+//        // 构造函数
+//        sol::constructors<GameObject(std::string, bool)>(),
+//
+//        // 成员变量
+//        "isActive", &GameObject::isActive,
+//        "name", &GameObject::name,
+//        "transform", &GameObject::transform,
+//        "components", &GameObject::components,
+//
+//        // 成员函数
+//        "getID", &GameObject::getID,
+//        "isRootGameObject", &GameObject::isRootGameObject
+//        );
+//
+//    lua["GameObject"]["addComponent"] = [](GameObject& obj, ComponentType type) {
+//        return obj.addComponent(type);
+//    };
+//
+//    lua["GameObject"]["getComponent"] = [](GameObject& obj, ComponentType type) {
+//        return obj.getComponent(type);
+//    };
+//
+//}
+
+//  Transform
+//void TransformLua(sol::state lua) {
+//    lua.open_libraries(sol::lib::base, sol::lib::package);
+//
+//    lua.new_usertype<Transform>("Transform",
+//        // 构造函数
+//        sol::constructors<Transform(GameObject*)>(),
+//
+//        // 成员变量
+//        "localPosition", &Transform::localPosition,
+//        "localRotation", &Transform::localRotation,
+//        "localScale", &Transform::localScale,
+//        "children", &Transform::children,
+//        "parent", &Transform::parent,
+//
+//        // 成员函数
+//        "getWorldPosition", &Transform::getWorldPosition,
+//        "getWorldRotation", &Transform::getWorldRotation,
+//        "getWorldScale", &Transform::getWorldScale,
+//        "translate", &Transform::translate
+//        );
+//
+//
+//}
---
+++ Core/CppcallLua/BindingLua.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <sol/sol.hpp>
+
+void bindAllClasses(sol::state& lua);
