05384c76ac1609cf976773f5fb005ea0498828aa
SoulerSakeR <53467643+SoulerSakeR@users.noreply.github.com>
2023-04-11 04:24:00 +0800

    player
     12

---
+++ Core/GameLogic/Player.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <Windows.h>
+#include "Player.h"
+
+void Player::move(float x, float y, float deltaTime) {
+	// 如果GameLoop中的inputDetection检测到了WASD的按键
+	// 进行相应的动作
+	// TODO:被LUA调用之后
+
+	this.
+}
+
+void Player::updateInfo(float deltatime) {
+}
--- Core/GameLogic/Player.h
+++ Core/GameLogic/Player.h
@@ -1,2 +1,34 @@
 #pragma once
 #include "GameLoop.h"
+#include "Core/Utils/Vector2D.h"
+#include "Core/Physics/GameObject.h"
+
+class Player {
+public:
+    Player(float x, float y) // 构造函数，初始化位置信息
+        : m_position(x, y), m_velocity(0.0f, 0.0f)
+    {}
+
+    // 运动信息
+    void move(float x, float y, float deltaTime);
+    void updateInfo(float deltatime);
+
+    // 状态信息
+    int getHealth() ;
+    void setHealth(int health);
+    int getScore() ;
+    void setScore(int score);
+    Vector2D getPosition();
+
+
+    // 与其他游戏对象的交互信息
+    void interactWithEnemy(GameObject& enemy);
+    void interactWithItem(GameObject& item);
+    void interactWithNPC(GameObject& npc);
+
+private:
+    Vector2D m_position; // 位置信息
+    Vector2D m_velocity; // 运动信息
+    int m_health;        // 状态信息
+    int m_score;         // 状态信息
+};
