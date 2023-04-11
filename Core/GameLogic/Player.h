2627aa58942ec2c5e3c05fa7dfbaf1966ce71683
SoulerSakeR <53467643+SoulerSakeR@users.noreply.github.com>
2023-04-12 01:36:59 +0800

    Player类
    123

--- Core/GameLogic/Player.h
+++ Core/GameLogic/Player.h
@@ -1,16 +1,20 @@
 #pragma once
 #include "GameLoop.h"
 #include "Core/Utils/Vector2D.h"
-#include "Core/Physics/GameObject.h"
-
+#include "Core/Core/GameObject.h"
+#include "Core/Utils/Vector2D.h"
 class Player {
 public:
-    Player(float x, float y) // 构造函数，初始化位置信息
-        : m_position(x, y), m_velocity(0.0f, 0.0f)
+    Player(GameObject* gameObject, float posX, float posY) // 构造函数，初始化位置信息
+        // 现在暂时
+        :
+        gameObject(gameObject),
+        m_position(0.0f, 0.0f),
+        m_velocity(0.0f, 0.0f)
     {}
 
     // 运动信息
-    void move(float x, float y, float deltaTime);
+    void move(float deltaTime);
     void updateInfo(float deltatime);
 
     // 状态信息 （游戏待定义）
@@ -20,7 +24,7 @@ public:
    // void setScore(int score);
 
     Vector2D getPosition();
-    void setPosition(float x, float y);
+    void setMoveValue(Vector2D value);
 
     Vector2D getVelocity();
     void setVelocity(float Vx, float Vy);
@@ -31,9 +35,18 @@ public:
    // void interactWithItem(GameObject& item);
    // void interactWithNPC(GameObject& npc);
 
+    int getHealth();
+    void setHealth(int health);
+
+    int getScore();
+    void setScore(int score);
+
 private:
-    Vector2D m_position; // 位置信息
+    Vector2D m_position; // 位置信息 玩家默认位置信息是在(0,0)
     Vector2D m_velocity; // 运动信息
+
     int m_health;        // 状态信息
     int m_score;         // 状态信息
+
+    GameObject* gameObject; //指向一个游戏物体
 };
