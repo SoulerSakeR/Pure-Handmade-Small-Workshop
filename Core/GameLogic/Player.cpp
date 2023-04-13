5f7e7e8cab02e4ec608045d12195f00648b3689e
SoulerSakeR <53467643+SoulerSakeR@users.noreply.github.com>
2023-04-13 23:16:13 +0800

    保存
    123

--- Core/GameLogic/Player.cpp
+++ Core/GameLogic/Player.cpp
@@ -4,6 +4,16 @@
 #include <Windows.h>
 #include "Player.h"
 
+/*
+1.玩家输入控制：包含处理玩家输入的代码，比如响应按键、鼠标点击等事件的处理。这些代码需要在游戏循环中实现。
+
+2.碰撞检测：实现与其他游戏对象的碰撞检测。如果玩家需要与其他游戏对象交互，例如避免与障碍物碰撞，就需要在游戏循环中实现相应的碰撞检测逻辑。
+
+3.状态信息：定义了健康值和分数等状态信息，但是还没有实现它们的具体逻辑。如果玩家需要在游戏中受到伤害或者得到分数，就需要在游戏循环中实现相应的逻辑。
+
+4.交互信息：实现与其他游戏对象的交互逻辑。如果玩家需要与敌人、物品或NPC交互，例如攻击敌人或者获得物品，就需要在游戏循环中实现相应的逻辑。
+*/
+
 
 // 设置玩家偏移值
 void Player::setMoveValue(Vector2D value) {
@@ -64,4 +74,9 @@ void Player::setScore(int score) {
 // 更新Player调用这个就对了
 void Player::updatePlayerInfo(float deltaTime) {
 	this->move(deltaTime);
+}
+
+// 检测玩家输入
+void Player::playerInputDetection(float deltaTime) {
+
 }
