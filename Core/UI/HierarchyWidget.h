587eb65c072d1fd4b95272d87b57243709d61b56
yuq-zhao <118006457+yuq-zhao@users.noreply.github.com>
2023-04-13 15:38:59 +0100

    UI拖拽1.0
    // TODO：左键点击出问题了

--- Core/UI/HierarchyWidget.h
+++ Core/UI/HierarchyWidget.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <qtreewidget.h>
 #include "HierarchyItem.h"
+#include "Core/UI/renderwindow.h"
 
 class HierarchyWidget :
     public QTreeWidget
@@ -10,14 +11,22 @@ signals:
     void gameObjectSelected(GameObject* gameobj);
 public:
     HierarchyWidget(QWidget* parent);
+    void mousePressEvent(QMouseEvent* event) override;
+
+    void mouseMoveEvent(QMouseEvent* event) override;
+
+    void mouseReleaseEvent(QMouseEvent* event) override;
 
     QMenu* contextMenu;
     GameObject* selectedGameObject;
+    // RenderWindow* ui;
 public slots:
     void showContextMenu(const QPoint& pos);
     void onSelectionChanged();
        
 private:
     void initContextMenu();
+    GameObject* gameObject;
+    QPoint startPos;
 };
 
